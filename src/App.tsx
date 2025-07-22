bb22161aee414c1d09a2153b7e84267254f5b937
Mario Alvarado <m_alvaradox@hotmail.com>
2025-07-22 08:47:07 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import { Grid } from '@mui/material';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
